c8cb4c24d40f85011f993bf47a24af0e9d050ca0
CYFly <chengyixiang@sogou-inc.com>
2016-08-08 17:35:30 +0800

    modified .rb

--- setup/ConfigureiOS.rb
+++ setup/ConfigureiOS.rb
@@ -12,10 +12,13 @@ module Pod
     end
 
     def perform
-=begin
-      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
+      keep_demo = :yes
+      #configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
 
-      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Specta", "Kiwi", "None"]).to_sym
+      framework = :none
+      configurator.set_test_framework("xctest", "m")
+      #configurator.ask_with_answers("Which testing frameworks will you use", ["Specta", "Kiwi", "None"]).to_sym
+=begin
       case framework
         when :specta
           configurator.add_pod_to_podfile "Specta"
@@ -34,7 +37,7 @@ module Pod
         when :none
           configurator.set_test_framework("xctest", "m")
       end
-
+=end
       snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
       case snapshots
         when :yes
--- setup/MessageBank.rb
+++ setup/MessageBank.rb
@@ -41,8 +41,8 @@ module Pod
 
       has_run_before = `defaults read org.cocoapods.pod-template HasRunbefore`.chomp == "1"
 
-      puts "If this is your first time we recommend running through with the guide: "
-      puts " - "  + "http://guides.cocoapods.org/making/using-pod-lib-create.html".blue.underline
+#puts "If this is your first time we recommend running through with the guide: "
+#puts " - "  + "http://guides.cocoapods.org/making/using-pod-lib-create.html".blue.underline
 
       if ENV["TERM_PROGRAM"] == "iTerm.app"
         puts " ( hold cmd and click links to open in a browser. )".magenta
@@ -70,7 +70,7 @@ module Pod
 
     def run_setup_questions
 
-      puts yellow_bang + "Before you can create a new library we need to setup your git credentials."
+      puts yellow_bang + "Before you can create a new project we need to setup your git credentials."
 
       unless @configurator.user_name.length > 0
         puts "\n What is your name? "
